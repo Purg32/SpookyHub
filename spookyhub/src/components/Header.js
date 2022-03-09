5a9872a0baa1094a901f16d11313878aa65f7191
Purg32 <kuba.piasta@hotmail.com>
2022-03-09 14:48:16 +0100

    Created necessary files

---
+++ spookyhub/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="inner-content">
+
+                </div>
+            </div>
+        </header>
+    )
+}
