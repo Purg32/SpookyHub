ea098e220118baf8ea2e7fa0444bdde3c80d86af
Purg32 <kuba.piasta@hotmail.com>
2022-03-11 13:27:36 +0100

    Made working add button

--- spookyhub/src/components/ResultCard.js
+++ spookyhub/src/components/ResultCard.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState";
 
 export const ResultCard = ({movie}) => {
+    const {addMovieToWatchlist} = useContext(GlobalContext);
+
     return (
         <div className="result=card">
             <div className="poster-wrapper">
@@ -22,7 +25,9 @@ export const ResultCard = ({movie}) => {
                 </div>
 
                 <div className="controls">
-                    <button className="btn">Add to Watchlist</button>
+                    <button className="btn" onClick={() => addMovieToWatchlist(movie)}>
+                        Add to Watchlist
+                    </button>
                 </div>
             </div>
         </div>
