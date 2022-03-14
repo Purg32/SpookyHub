0e341498a7b77f8a15863b443f332ce50f1c664f
Purg32 <kuba.piasta@hotmail.com>
2022-03-14 14:36:39 +0100

    Working watchlist and watched page

--- spookyhub/src/components/ResultCard.js
+++ spookyhub/src/components/ResultCard.js
@@ -2,11 +2,14 @@ import React, {useContext} from "react";
 import {GlobalContext} from "../context/GlobalState";
 
 export const ResultCard = ({movie}) => {
-    const {addMovieToWatchlist, watchlist} = useContext(GlobalContext);
+    const {addMovieToWatchlist, addMovieToWatched, watchlist, watched} = useContext(GlobalContext);
 
-    let storedMovie = watchlist.find(o => o.id === movie.id);
+    let storedMovie = watchlist.find((o) => o.id === movie.id);
+    let storedMovieWatched = watched.find((o) => o.id === movie.id);
 
-    const watchlistDisabled = storedMovie ? true : false;
+    const watchlistDisabled = storedMovie ? true : storedMovieWatched ? true: false;
+
+    const watchedDisabled = storedMovieWatched ? true : false;
 
     return (
         <div className="result=card">
@@ -33,6 +36,12 @@ export const ResultCard = ({movie}) => {
                             disabled={watchlistDisabled}
                             onClick={() => addMovieToWatchlist(movie)}>
                         Add to Watchlist
+                    </button>
+
+                    <button className="btn"
+                            disabled={watchlistDisabled}
+                            onClick={() => addMovieToWatched(movie)}>
+                        Add to Watched
                     </button>
                 </div>
             </div>
--- spookyhub/src/context/GlobalState.js
+++ spookyhub/src/context/GlobalState.js
@@ -22,8 +22,27 @@ export const GlobalProvider = props => {
         dispatch({type: "ADD_MOVIE_TO_WATCHLIST", payload: movie});
     };
 
+    const removeMovieFromWatchlist = (id) => {
+        dispatch({type: "REMOVE_MOVIE_FROM_WATCHLIST", payload: id});
+    };
+
+    const addMovieToWatched = movie => {
+        dispatch({type: "ADD_MOVIE_TO_WATCHED", payload: movie});
+    };
+
+    const moveToWatchlist = movie => {
+        dispatch({type: "MOVE_TO_WATCLIST", payload: movie});
+    };
+
     return (
-        <GlobalContext.Provider value={{watchlist: state.watchlist, watched: state.watched, addMovieToWatchlist,}}>
+        <GlobalContext.Provider value={{
+            watchlist: state.watchlist,
+            watched: state.watched,
+            addMovieToWatchlist,
+            removeMovieFromWatchlist,
+            addMovieToWatched,
+            moveToWatchlist,
+        }}>
             {props.children}
         </GlobalContext.Provider>
     )
