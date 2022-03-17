e3dbcfddfd6fee0b1d878862d594dec52f384966
Purg32 <kuba.piasta@hotmail.com>
2022-03-17 12:46:19 +0100

    added logo and added Burger Menu

--- spookyhub/src/components/Header.js
+++ spookyhub/src/components/Header.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 
 export const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+
+    }, [])
+
     return (
         <header>
             <div className="container">
                 <div className="inner-content">
                     <div className="brand">
-                        <Link to="/">SpookyHub</Link>
+                        <Link to="/"><img className="logo" src={logo}/></Link>
                     </div>
 
+                    {(toggleMenu || screenWidth > 700) && (
                     <ul className="nav-links">
                         <li>
                             <Link to="/">To-Watch List</Link>
@@ -29,8 +53,13 @@ export const Header = () => {
                             </Link>
                         </li>
                     </ul>
+                    )}
+                    <button onClick={toggleNav} className="btn-burger">
+                        <i className="fa-fw fa fa-bars"></i>
+                    </button>
                 </div>
             </div>
+
         </header>
     );
 };
